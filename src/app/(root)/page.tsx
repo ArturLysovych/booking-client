1aa6fc11798e3cdf68222b7795aff65ce9b788df
ArturLysovych <arturlisovic@gmail.com>
2024-03-24 13:23:46 +0200

    initial commit

---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen">
+      <form>
+        <input type="text" />
+        <input type="date" />
+        <button>Submitx</button>
+      </form>
+    </div>
+  );
+}
